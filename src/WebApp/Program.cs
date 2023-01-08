fe7bc56444ea22704c4b0a3ec9727c578e9ef172
akachan <akachan@adirecruitment.com>
2023-01-08 22:35:47 +0700

    add log

--- src/WebApp/Program.cs
+++ src/WebApp/Program.cs
@@ -1,3 +1,5 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+using var log = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File("Logs/log.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+log.Information("Start app dotnet!");
 
 var app = builder.Build();
 
